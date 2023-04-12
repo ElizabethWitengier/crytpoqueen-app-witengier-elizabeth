753749a144c66f3570f4fa15ce5b26aa3a84e3d9
Elizabeth Witengier <ewitengier@ufl.edu>
2023-04-12 18:23:32 -0400

    Search Page Update

---
+++ src/pages/search.js
@@ -0,0 +1,16 @@
+import Coins from "@/components/Coins";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const search = () => {
+  return (
+    <div>
+      <Navbar />
+      <Coins />
+      <Footer />
+    </div>
+  );
+};
+
+export default search;
