d9622296566d8e1430db28259957261477854cb8
Elizabeth Witengier <ewitengier@ufl.edu>
2023-04-13 17:26:25 -0400

    Copy Added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,12 +6,20 @@ const Hero = () => {
       <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7">
           <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-            Payments tool for software companies
+            Ready to get started with cryptocurrency?
           </h1>
+
           <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-            From checkout to global sales tax compliance, companies around the
-            world use Flowbite to simplify their payment stack.
+            SheCoin is a platform designed to help women learn about and invest
+            in cryptocurrency. Our mission is to empower women to take control
+            of their finances and participate in the exciting crypto world.
           </p>
+          <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            Join our community and connect with other women learning and
+            investing in crypto. Together, we can empower each other to achieve
+            financial independence and security.
+          </p>
+
           <a
             href="#"
             class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -6,17 +6,26 @@ const Videos = () => {
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div class="block w-100 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
+            5 Things to Know If You Are New to Cryptocurrency{" "}
           </h5>
           <p class="font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+            If you're new to cryptocurrency, it's important to start small and
+            only use money you can afford to lose. Cryptocurrency offers many
+            advantages but is still volatile and risky for inexperienced
+            investors. Be prepared for volatility, and consider venturing into
+            other altcoins besides Bitcoin. It's also crucial to have a strategy
+            for crypto trading, manage risk, diversify your portfolio, be in it
+            for the long term, and automate purchases. Avoid common
+            cryptocurrency mistakes such as buying just because the price is low
+            and going "all-in." Finally, stay vigilant, keep an eye on reputable
+            news sources, and avoid being swayed by hype or quick money
+            promises.
           </p>
         </div>
         <div class="aspect-video">
           <iframe
             class="w-full h-full"
-            src="https://www.youtube.com/embed/FF3fuYLnApQ"
+            src="https://www.youtube.com/embed/21-SQN5ZfM0"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
@@ -24,17 +33,28 @@ const Videos = () => {
         </div>
         <div class="block w-100 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
+            Pros and Cons of Cryptocurrency: A Beginner's Guide
           </h5>
           <p class="font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+            Cryptocurrency offers several benefits, including diversification
+            from traditional financial assets, fast transaction speeds, low
+            transaction costs, and direct investment control. Additionally,
+            cryptocurrencies provide accessibility for the unbanked, high levels
+            of security through the blockchain system, user privacy, and
+            transparency with all transactions recorded on the blockchain
+            ledger. However, it's important to note that cryptocurrencies also
+            come with risks, such as the potential to lose funds if private keys
+            are lost or stolen. It's recommended to start small and only use
+            money that can afford to lose, as the market is volatile and risky
+            for inexperienced investors. Diversifying one's portfolio, having a
+            strategy for trading, and staying vigilant against common mistakes
+            and hype are also essential factors to consider.
           </p>
         </div>
         <div class="aspect-video">
           <iframe
             class="w-full h-full"
-            src="https://www.youtube.com/embed/FF3fuYLnApQ"
+            src="https://www.youtube.com/embed/Ab3cr-hJ3Ko"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
