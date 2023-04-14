3616a2ec9607258888d9ee943b2100a3331d78ef
Elizabeth Witengier <ewitengier@ufl.edu>
2023-04-14 18:32:46 -0400

    Login and Signup

---
+++ src/pages/api/login.js
@@ -0,0 +1,27 @@
+import { connectToDatabase } from "../../utils/mongodb";
+import bcrypt from "bcrypt";
+
+export default async function handler(req, res) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please provide email and password" });
+  }
+
+  const { db } = await connectToDatabase();
+
+  const user = await db.collection("users").findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+
+  if (!isValidPassword) {
+    return res.status(401).json({ message: "Invalid password" });
+  }
+  res.status(200).json(user);
+}
---
+++ src/pages/api/signup.js
@@ -0,0 +1,34 @@
+import { connectToDatabase } from "../../utils/mongodb";
+import bcrypt from "bcrypt";
+
+export default async function handler(req, res) {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please provide name, email, and password" });
+  }
+
+  const { db } = await connectToDatabase();
+
+  const existingUser = await db.collection("users").findOne({ email });
+
+  if (existingUser) {
+    return res
+      .status(409)
+      .json({ message: "User with this email already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await db.collection("users").insertOne({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  // Create a session token or cookie and return it to the client...
+
+  res.status(201).json(user);
+}
