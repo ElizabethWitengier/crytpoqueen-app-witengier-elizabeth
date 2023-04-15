4be5cb01efaabe6e29d07b21c64193f8cb1ae849
Elizabeth Witengier <ewitengier@ufl.edu>
2023-04-15 16:12:32 -0400

    Updating Login

---
+++ src/pages/login.js
@@ -0,0 +1,16 @@
+import Footer from "@/components/Footer";
+import LoginForm from "@/components/LoginForm";
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <Navbar />
+      <LoginForm />
+      <Footer />
+    </div>
+  );
+};
+
+export default Login;
